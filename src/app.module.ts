00b270fa32b088b75916fb2db57956defef79656
gongonut <gongonut@gmail.com>
2025-08-27 14:41:22 -0500

    refactor: Disable database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -11,7 +11,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       rootPath: join(__dirname, '..', 'apps', 'IBA_noche'),
       serveRoot: '/IBA_noche',
     }),
-    MongooseModule.forRoot('mongodb://localhost/test'),
+    // MongooseModule.forRoot('mongodb://localhost/test'),
   ],
   controllers: [AppController],
   providers: [AppService],
